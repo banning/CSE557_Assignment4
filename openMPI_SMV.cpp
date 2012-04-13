75c5ca43cabcee45c71b2ddbc243314647b038be
Mike Banning <banning@psu.edu>
2012-04-12 23:15:54 -0400

    Initial upload

---
+++ openMPI_SMV.cpp
@@ -0,0 +1,28 @@
+// CSE557 Assignment 4
+// Calculate sparse matrix vector multiplication
+// 04/27/12
+// Mike Banning
+// Dan Keating
+
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <iostream>
+using namespace std;
+
+
+int main (int argc, char *argv[])
+{
+	//Find matrix to load from input
+
+	//Load CSR Matrix
+
+	//Use larger of row/col for vector length
+	
+	//Create Vector
+
+	//SMV Algorithm
+
+	return 0;
+}
