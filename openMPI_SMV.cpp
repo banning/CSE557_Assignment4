c65aae9c71e17f5a2d64db573f3dbe2cb2f6277b
dsk5026 <dsk5026@psu.edu>
2012-04-16 21:41:01 -0300

    Update openMPI_SMV.cpp

--- openMPI_SMV.cpp
+++ openMPI_SMV.cpp
@@ -1,28 +1,92 @@
-// CSE557 Assignment 4
-// Calculate sparse matrix vector multiplication
-// 04/27/12
-// Mike Banning
-// Dan Keating
-
 #include <omp.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
-#include <iostream>
 using namespace std;
-
-
-int main (int argc, char *argv[])
+struct matrix
 {
-	//Find matrix to load from input
-
-	//Load CSR Matrix
-
-	//Use larger of row/col for vector length
-	
-	//Create Vector
-
-	//SMV Algorithm
-
-	return 0;
-}
+   vector<int> row;
+   vector<int> col;
+   vector<double> val;
+   int n;
+   int num;
+};
+int main(int argc, const char* argv[])
+{
+   if (argc <2)
+   {
+      cout<<"Correct format is ./matrixsum inputfile.csr"<<endl;
+      return 0;
+   }
+   string line;
+   matrix A;
+   int last=0;
+   ifstream myFile(argv[1]);
+   if (myFile.is_open())
+   {
+      //gets rid of documentation line
+      getline(myFile, line);
+      getline(myFile, line);
+      int space = line.find_first_of(" ");
+      A.n = atoi(line.substr(0, space).c_str());
+      A.num=atoi(line.substr(space+1, line.length()-space).c_str());
+      while (myFile.good())
+      {
+         getline(myFile, line);
+         space = atoi(line.c_str());
+         if (space < last && last != 0)
+         {
+            A.col.push_back(space);
+            break;
+         }
+         last=space;
+         A.row.push_back(space);
+      }
+      for (int i = 0; i < A.num-1; i++)
+      {
+         getline(myFile, line);
+         space=atoi(line.c_str());
+         A.col.push_back(space);
+      }
+      for (int i = 0; i < A.num; i++)
+      {
+         getline(myFile, line);
+         double val=atof(line.c_str());
+         A.val.push_back(val);
+      }
+   }
+   cout<<"Values"<<endl;
+   for (int i = 0; i < A.num; i++)
+   {
+      cout<<A.val.at(i)<<'\t';
+   }
+   cout<<endl;
+   
+   int x[A.n];
+   double y[A.n];
+   for (int i = 0;  i < A.num; i++)
+   {
+      x[i]=1;
+   }
+   double sum;
+   cout<<"Running sum now"<<endl;
+   for (int i = 0; i < A.n; i++)
+   {
+      sum = 0.0;  
+      cout<<"J goes from "<<A.row.at(i)<< " to " <<A.row.at(i+1)-1<<endl;
+      for (int j = A.row.at(i)-1; j <  A.row.at(i+1)-1; j++)
+      {
+         double Xi = x[A.col.at(j)];
+         double Ai = A.val.at(j);
+         sum = sum + Ai*Xi;
+      }
+      y[i]=sum;
+   }
+   for (int i = 0; i < A.n; i++)
+   {
+      cout <<"y["<<i<<"] = "<<y[i]<<endl;
+   }
+   return 0;
+} 
